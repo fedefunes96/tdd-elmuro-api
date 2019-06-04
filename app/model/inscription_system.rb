e3f84192e7ad1f54f838f278bf27453bac04abb4
Funes Federico <fede.funes96@gmail.com>
2019-06-04 04:45:18 -0300

    InscriptionSystem now can check if there is an inscription of a student in a subject

--- app/model/inscription_system.rb
+++ app/model/inscription_system.rb
@@ -1,7 +1,21 @@
 require_relative 'inscription'
+require_relative '../exceptions/invalid_inscription'
 
 class InscriptionSystem
+  def initialize
+    @inscriptions = []
+  end
+
   def create_inscription(student, subject)
-    Inscription.new student, subject
+    inscription = (Inscription.new student, subject)
+    @inscriptions << inscription
+    inscription
+  end
+
+  def inscripted_to?(student, subject)
+    @inscriptions.any? do |inscription|
+      inscription.subject.code == subject.code &&
+        inscription.student.username == student.username
+    end
   end
 end
--- spec/model/inscription_system_spec.rb
+++ spec/model/inscription_system_spec.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require_relative '../../app/model/student'
 require_relative '../../app/model/subject'
 require_relative '../../app/model/inscription_system'
+require_relative '../../app/exceptions/invalid_inscription'
 
 describe InscriptionSystem do
   let(:student) { Student.new('Juan Perez', 'juanperez') }
@@ -12,5 +13,12 @@ describe InscriptionSystem do
     inscription = inscription_system.create_inscription(student, subject1)
 
     expect(inscription.subject.name).to eq('memo2')
+    expect(inscription.student.name).to eq('Juan Perez')
+  end
+
+  it 'should know if a student is inscripted to a subject' do
+    inscription_system.create_inscription(student, subject1)
+
+    expect(inscription_system.inscripted_to?(student, subject1)).to eq(true)
   end
 end
