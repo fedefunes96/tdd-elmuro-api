d5fb65aab807c6df3d46596f1ada1f036d5acafa
Funes Federico <fede.funes96@gmail.com>
2019-06-04 01:15:34 -0300

    Students can inscript to subjects

--- app/model/student.rb
+++ app/model/student.rb
@@ -1,8 +1,13 @@
 class Student
-  attr_accessor :name, :username
+  attr_accessor :name, :username, :inscriptions
 
   def initialize(name, username)
     @name = name
     @username = username
+    @inscriptions = []
+  end
+
+  def inscript(subject)
+    @inscriptions << subject.code
   end
 end
--- spec/model/student_spec.rb
+++ spec/model/student_spec.rb
@@ -1,8 +1,10 @@
 require 'rspec'
 require_relative '../../app/model/student'
+require_relative '../../app/model/subject'
 
 describe Student do
   let(:student) { described_class.new('Juan Perez', 'juanperez') }
+  let(:subject1) { Subject.new('memo2', '9521', 'NicoPaez', 30, false, false) }
 
   describe 'attributes' do
     it 'should respond to name' do
@@ -12,5 +14,11 @@ describe Student do
     it 'should respond to username' do
       expect(student.username).to eq('juanperez')
     end
+  end
+
+  it 'should inscript to a subject' do
+    student.inscript(subject1)
+
+    expect(student.inscriptions.include?('9521')).to eq(true)
   end
 end
