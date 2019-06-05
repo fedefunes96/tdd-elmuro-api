802370b07cd024fb74e18d984345441b0e4a2518
Funes Federico <fede.funes96@gmail.com>
2019-06-05 05:08:49 -0300

    Using joins instead of reading database too much times in InscriptionRepository

--- repositories/inscription_repository.rb
+++ repositories/inscription_repository.rb
@@ -8,11 +8,20 @@ class InscriptionRepository < BaseRepository
   def all_inscriptions
     inscriptions = []
 
-    dataset.each do |row|
-      student = StudentRepository.new.find_by_username(row[:username])
-      subject = SubjectRepository.new.find_by_code(row[:subject_code])
+    StudentRepository.new.all.join(dataset, username: :username)
+                     .join(SubjectRepository.new.all, code: :subject_code)
+                     .each do |row|
+      student = retrieve_student(row)
+      subject = retrieve_subject(row)
+
       inscriptions << (Inscription.new student, subject)
     end
+
+    # dataset.each do |row|
+    #   student = StudentRepository.new.find_by_username(row[:username])
+    #   subject = SubjectRepository.new.find_by_code(row[:subject_code])
+    #   inscriptions << (Inscription.new student, subject)
+    # end
 
     inscriptions
   end
@@ -29,5 +38,20 @@ class InscriptionRepository < BaseRepository
       username: inscription.student.username,
       subject_code: inscription.subject.code
     }
+  end
+
+  private
+
+  def retrieve_student(row)
+    Student.new(row[:name], row[:username])
+  end
+
+  def retrieve_subject(row)
+    Subject.new(row[:name],
+                row[:code],
+                row[:teacher],
+                row[:max_students],
+                row[:projector],
+                row[:laboratory])
   end
 end
