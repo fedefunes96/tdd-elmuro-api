ebbd98069f35dc06f1fc5122faf158e1236c7941
Funes Federico <fede.funes96@gmail.com>
2019-06-05 00:31:13 -0300

    post to /inscribir now saves inscriptions in database

--- app/controller/inscription_controller.rb
+++ app/controller/inscription_controller.rb
@@ -5,14 +5,48 @@ class InscriptionController
 
   SUCCESS_MESSAGE = 'inscripcion_creada'.freeze
   PARAMETER_MISSING = 'parametro_faltante'.freeze
+  SUBJECT_MISSING = 'materia_faltante'.freeze
 
   def create(body)
     return api_response(PARAMETER_MISSING), 400 unless all_params?(body)
 
-    [api_response(SUCCESS_MESSAGE), 201]
+    message, status = create_inscription(body)
+    [api_response(message), status]
   end
 
   private
+
+  def create_inscription(body)
+    begin
+      subject = SubjectRepository.new.find_by_code(body[CODE])
+
+      return SUBJECT_MISSING, 400 if subject.nil?
+
+      student = retrieve_student(body)
+
+      inscription = InscriptionSystem.new.create_inscription(student, subject)
+    rescue GuaraniError => e
+      return error_msg(e), 400
+    end
+    InscriptionRepository.new.save(inscription)
+    [SUCCESS_MESSAGE, 201]
+  end
+
+  def retrieve_student(body)
+    student = Student.new body[NAME], body[USERNAME]
+
+    StudentRepository.new.save(student)
+
+    student
+  end
+
+  def error_msg(error)
+    messages = {
+      InvalidInscription => 'inscripcion_invalida'
+    }
+
+    messages[error.class]
+  end
 
   def all_params?(body)
     body.include?(NAME) && body.include?(CODE) && body.include?(USERNAME)
--- spec/inscriptions_spec.rb
+++ spec/inscriptions_spec.rb
@@ -9,6 +9,13 @@ describe 'Inscripcion alumnos' do
     Sinatra::Application
   end
 
+  let(:student) { Student.new('Juan Perez', 'juanperez') }
+  let(:subject1) { Subject.new('Tecnicas 2', '7592', 'NicoPaez', 10, true, false) }
+
+  before(:each) do
+    SubjectRepository.new.save(subject1)
+  end
+
   it 'accepts a post to /inscribir' do
     post_with_body('/inscribir', {})
     expect(last_response.status).not_to eq 404
@@ -18,5 +25,14 @@ describe 'Inscripcion alumnos' do
     post_with_body('/inscribir', nombre_completo: 'Juan Perez',
                                  username_alumno: 'juanperez', codigo_materia: '7592')
     expect(last_response.status).to eq 201
+  end
+
+  it 'new inscription created is persisted on db' do
+    post_with_body('/inscribir', nombre_completo: 'Juan Perez',
+                                 username_alumno: 'juanperez', codigo_materia: '7592')
+
+    inscriptions = InscriptionRepository.new.all_inscriptions
+
+    expect(inscriptions.include?(Inscription.new(student, subject1))).to eq true
   end
 end
