70f31910ec902968371ad882edece05fc0d2951d
Lucas Lavandeira <lucaslavandeira@gmail.com>
2019-06-09 22:39:26 -0300

    Refactor grader class

--- app/model/graders/finals_grader.rb
+++ app/model/graders/finals_grader.rb
@@ -2,20 +2,24 @@ class FinalsGrader
   MAX_GRADES = 1
   MAX_GRADE_VALUE = 10
 
-  def validate!(grades)
-    raise InvalidGradeError if grades.size > MAX_GRADES
-
-    grades.each { |x| raise InvalidGradeError unless valid_grade?(x) }
+  def initialize(grades)
+    validate!(grades)
+    @grades = grades
   end
 
-  def final_grade(grades)
-    validate!(grades)
-    grades.first
+  def final_grade
+    @grades.first
   end
 
   private
 
   def valid_grade?(grade)
     grade.is_a?(Numeric) && grade.positive? && grade <= MAX_GRADE_VALUE
+  end
+
+  def validate!(grades)
+    raise InvalidGradeError if grades.size > MAX_GRADES
+
+    grades.each { |x| raise InvalidGradeError unless valid_grade?(x) }
   end
 end
--- spec/model/graders/finals_grader_spec.rb
+++ spec/model/graders/finals_grader_spec.rb
@@ -4,33 +4,28 @@ require_relative '../../../app/model/graders/finals_grader'
 require_relative '../../../app/exceptions/invalid_grade_error'
 
 describe FinalsGrader do
-  let(:grader) { described_class.new }
-
   it 'accepts a single grade as valid' do
-    grader.validate!([10])
+    described_class.new([10])
   end
 
   it 'rejects more than one grade as invalid' do
-    expect { grader.validate!([10, 10]) }.to raise_error(InvalidGradeError)
+    expect { described_class.new([10, 10]) }.to raise_error(InvalidGradeError)
   end
 
   it 'rejects a non numeric grade as invalid' do
-    expect { grader.validate!(['aprobado']) }.to raise_error(InvalidGradeError)
+    expect { described_class.new(['aprobado']) }.to raise_error(InvalidGradeError)
   end
 
   it 'rejects a negative grade as invalid' do
-    expect { grader.validate!([-1]) }.to raise_error(InvalidGradeError)
+    expect { described_class.new([-1]) }.to raise_error(InvalidGradeError)
   end
 
   it 'rejects a grade above 10 as invalid' do
-    expect { grader.validate!([11]) }.to raise_error(InvalidGradeError)
+    expect { described_class.new([11]) }.to raise_error(InvalidGradeError)
   end
 
   it 'final grade for [6] grades is 6' do
-    expect(grader.final_grade([6])).to eq 6
+    expect(described_class.new([6]).final_grade).to eq 6
   end
-
-  it 'invalid grades for final_grade throws error' do
-    expect { grader.final_grade([-1]) }.to raise_error(InvalidGradeError)
-  end
+  4
 end
