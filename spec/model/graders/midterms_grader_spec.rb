ad4148af458b508125ddb56ea290eda1ed12efed
Guille <guillecondori19@gmail.com>
2019-06-11 14:39:40 -0300

    added test in model if less than 2 grades is passed

--- spec/model/graders/midterms_grader_spec.rb
+++ spec/model/graders/midterms_grader_spec.rb
@@ -3,8 +3,12 @@ require_relative '../../../app/model/graders/midterms_grader'
 require_relative '../../../app/exceptions/invalid_grade_error'
 
 describe MidtermsGrader do
-  it 'should raise error when the numer of grades is not 2' do
+  it 'should raise error when the number of grades is above 2' do
     expect { described_class.new([7, 10, 8]) }.to raise_error(InvalidGradeError)
+  end
+
+  it 'should raise error when number of grades is less than 2' do
+    expect { described_class.new([7]) }.to raise_error(InvalidGradeError)
   end
 
   it 'should return mean of grades as final grade' do
